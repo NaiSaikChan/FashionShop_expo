9f52e35d151826605fcc780e655c01d9ab386f8b
Saik Chan <maraohnonpon@hotmail.com>
2024-08-18 14:27:16 +0700

    Create New Project and Add image.

--- app/(app)/(tabs)/(home)/index.tsx
+++ app/(app)/(tabs)/(home)/index.tsx
@@ -1,13 +1,25 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, Platform, Dimensions, Text, View} from 'react-native';
+import { Link, useNavigation } from 'expo-router';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const {width, height} = Dimensions.get("window");
 
 export default function HomeScreen() {
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({ headerShown: false});
+  }, [navigation]);
+
   return (
-    <ParallaxScrollView
+    <SafeAreaView style={styles.container}>
+        <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
@@ -47,6 +59,8 @@ export default function HomeScreen() {
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
+    </SafeAreaView>
+    
   );
 }
 
@@ -67,4 +81,8 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  container: {
+    minHeight: height,
+    backgroundColor: '#ffffff'
+  }
 });
